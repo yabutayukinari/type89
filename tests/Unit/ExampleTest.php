af9ec922f163ae3cccaad8db2dfb426b69feb8ea
YabutaYukinari <yabutayukinari@gmail.com>
2021-10-08 23:16:18 +0900

    ExampleTest.php追加

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
